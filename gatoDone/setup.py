3a4ad31f162134303a691663ae7648f5681ead86
Pedro Gonzalez <162183814+P-E-D-R-O-Gonzalez@users.noreply.github.com>
2026-05-20 16:45:30 -0700

    Delete gatoDone/setup.py

--- gatoDone/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setuptools.setup(     
-     name="gatoDone",     
-     version="0.0.1",
-     packages=["gatoDone"],
-     install_requires["playsound"],
-     package_dat={"gatoDone":["*.mp3"]},
-)
